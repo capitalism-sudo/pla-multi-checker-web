fb6756ee987f8707ac6d6f8b2e4e33ac8c05592e
unknown <adam.i.moskowitz@gmail.com>
2022-04-17 01:09:58 -0400

    fixed gender ratio in corner case of val == ratio

--- pla/checkseed.py
+++ pla/checkseed.py
@@ -650,7 +650,7 @@ def read_bonus_pathinfo(paths,rolls,group_seed,map_name,
                 display[index]["sprite"] = spritename
                 ratioarray = RATIOS[str(SPECIES.index(cutspecies))]
                 ratio = ratioarray[2]
-                if display[index]["gender"] <= ratio and cutspecies not in ["Bronzor", "Bronzong", "Rotom", "Voltorb", "Electrode", "Unown"]:
+                if display[index]["gender"] < ratio and cutspecies not in ["Bronzor", "Bronzong", "Rotom", "Voltorb", "Electrode", "Unown"]:
                     display[index]["gender"] = "Female <i class='fa-solid fa-venus' style='color:pink'></i>"
                 elif cutspecies in ["Bronzor", "Bronzong", "Rotom", "Voltorb", "Electrode","Unown"]:
                     display[index]["gender"] = "Genderless <i class='fa-solid fa-genderless'></i>"
@@ -709,7 +709,7 @@ def check_from_seed(group_seed,rolls,frencounter,brencounter,bonus_flag=False,ma
             ratioarray = RATIOS[str(SPECIES.index(cutspecies))]
             ratio = ratioarray[2]
             #print(f"Ratio: {ratio}")
-            if display[str(index)]["gender"] <= ratio and cutspecies not in ["Bronzor", "Bronzong", "Rotom", "Voltorb", "Electrode", "Unown"]:
+            if display[str(index)]["gender"] < ratio and cutspecies not in ["Bronzor", "Bronzong", "Rotom", "Voltorb", "Electrode", "Unown"]:
                 display[str(index)]["gender"] = "Female <i class='fa-solid fa-venus' style='color:pink'></i>"
             elif cutspecies in ["Bronzor", "Bronzong", "Rotom", "Voltorb", "Electrode","Unown"]:
                 display[str(index)]["gender"] = "Genderless <i class='fa-solid fa-genderless'></i>"
