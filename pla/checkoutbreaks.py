43b352bb3c3b942ff985dd04fb0b945219677376
unknown <adam.i.moskowitz@gmail.com>
2022-05-18 14:01:30 -0400

    outbreaks are seperate now

--- pla/checkoutbreaks.py
+++ pla/checkoutbreaks.py
@@ -1,9 +1,9 @@
 import struct
-from pla.core import BASE_ROLLS_OUTBREAKS, generate_from_seed, get_rolls, get_sprite
-from pla.data import pokedex, natures
-from pla.rng import XOROSHIRO
+from .core import BASE_ROLLS_OUTBREAKS, generate_from_seed, get_rolls, get_sprite
+from .data import pokedex, natures
+from .rng import XOROSHIRO
 
-from pla.checkmmo import MAX_MAPS, get_group_seed, get_gen_seed_to_group_seed
+from .checkmmo import MAX_MAPS, get_group_seed, get_gen_seed_to_group_seed
 
 def get_all_outbreaks(reader, research, inmap, rolls_override = None):
     """reads all normal outbreaks on map"""
