47bb6d1f10a2984849e3d8201f4c5f6edb119186
Adam Moskowitz <adam.i.moskowitz@gmail.com>
2023-02-25 21:23:00 -0500

    Missed a resource path

--- gen3/data/data.py
+++ gen3/data/data.py
@@ -1,4 +1,4 @@
-RESOURCE_PATH = './static/'
+from ...app import RESOURCE_PATH
 
 with open(RESOURCE_PATH + "resources/text_natures.txt",encoding="utf-8") as text_natures:
     _natures = text_natures.read().split("\n")
